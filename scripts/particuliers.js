3c570327f6393202c72a6f3d9840a261285aa5cf
YanisDj <101258286+YanisDj@users.noreply.github.com>
2023-04-25 12:05:42 +0200

    réorganisation des fichiers

---
+++ scripts/particuliers.js
@@ -0,0 +1,32 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000, // temps d'attente entre chaque diapositive en millisecondes
+        disableOnInteraction: false // arrête l'autoplay lorsqu'un utilisateur interagit avec le carrousel
+    }
+  });
+
+const menuHamburger = document.querySelector(".menu-hamburger")
+const navLinks = document.querySelector(".nav-links")
+
+menuHamburger.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+});
