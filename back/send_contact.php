b1051c6f78c375faa949b4497a6caced38c2df20
YanisDj <101258286+YanisDj@users.noreply.github.com>
2023-06-05 15:46:08 +0200

    probleme avec les formulaires

--- back/send_contact.php
+++ back/send_contact.php
@@ -1,30 +1,45 @@
 <?php
 if (isset($_POST['submit'])) {
-    $to = 'devis@prevoyanceservices.fr';
-    $subject = htmlentities($_POST['sujet'], ENT_QUOTES);
-    $name = htmlentities($_POST['name'], ENT_QUOTES);
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $message = htmlentities($_POST['message'], ENT_QUOTES);
-    $phone = htmlentities($_POST['telephone'], ENT_QUOTES);
-    
-    // Vérification des champs obligatoires
-    if (empty($subject) || empty($name) || empty($email) || empty($message) || empty($phone)) {
-        // Vous pouvez également rediriger l'utilisateur vers le formulaire avec un message d'erreur, si nécessaire.
-        echo 'error';
-        exit; // Arrête l'exécution du script si des champs sont manquants.
-    }
-    
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+        $to = 'devis@prevoyanceservices.fr';
+        $subject = htmlentities($_POST['sujet'], ENT_QUOTES);
+        $name = htmlentities($_POST['name'], ENT_QUOTES);
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $message = htmlentities($_POST['message'], ENT_QUOTES);
+        $phone = htmlentities($_POST['telephone'], ENT_QUOTES);
 
-    $body = "Nom: $name <br> Email: $email <br> Téléphone: $phone <br> Message: <br> $message";
+        // Vérification des champs obligatoires
+        if (empty($subject) || empty($name) || empty($email) || empty($message) || empty($phone)) {
+            http_response_code(400); // Code de réponse HTTP 400 (Bad Request)
+            exit; // Arrête l'exécution du script si des champs sont manquants.
+        }
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo 'success';
+        // Validation de l'e-mail
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            http_response_code(350); // Code de réponse HTTP 400 (Bad Request)
+            exit; // Arrête l'exécution du script si l'e-mail est invalide.
+        }
+
+        // Validation du numéro de téléphone
+        if (!preg_match('/^[0-9]{10}$/', $phone)) {
+            http_response_code(370); // Code de réponse HTTP 400 (Bad Request)
+            exit; // Arrête l'exécution du script si le numéro de téléphone est invalide.
+        }
+
+        $headers = "From: $name <$email>\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+        $body = "Nom: $name <br> Email: $email <br> Téléphone: $phone <br> Message: <br> $message";
+
+        if (mail($to, $subject, $body, $headers)) {
+            http_response_code(200); // Code de réponse HTTP 200 (OK)
+        } else {
+            http_response_code(500); // Code de réponse HTTP 500 (Internal Server Error)
+        }
     } else {
-        echo 'error';
+        http_response_code(400); // Code de réponse HTTP 400 (Bad Request)
+        exit; // Arrête l'exécution du script si la validation reCAPTCHA échoue.
     }
-}
+
 ?>
--- back/send_santé.php
+++ back/send_santé.php
@@ -1,14 +1,31 @@
 <?php
 
-require_once 'PHPMailer-5.2-stable/PHPMailerAutoload.php';
 if (isset($_POST['submit'])) {
     $to = 'contact@prevoyanceservices.com';
     $subject = 'Devis santé';
     $name = htmlentities($_POST['name'], ENT_QUOTES);
-    $from = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $message = htmlentities($_POST['message'], ENT_QUOTES);
     $phone = htmlentities($_POST['telephone'], ENT_QUOTES); 
 
+     // Vérification des champs obligatoires
+     if (empty($subject) || empty($name) || empty($email) || empty($message) || empty($phone)) {
+        http_response_code(400); // Code de réponse HTTP 400 (Bad Request)
+        exit; // Arrête l'exécution du script si des champs sont manquants.
+    }
+
+    // Validation de l'e-mail
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        http_response_code(350); // Code de réponse HTTP 400 (Bad Request)
+        exit; // Arrête l'exécution du script si l'e-mail est invalide.
+    }
+    
+    // Validation du numéro de téléphone
+    if (!preg_match('/^[0-9]{10}$/', $phone)) {
+        http_response_code(370); // Code de réponse HTTP 400 (Bad Request)
+        exit; // Arrête l'exécution du script si le numéro de téléphone est invalide.
+    }
+    
     $headers = "From: $name <$email>\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
@@ -17,9 +34,9 @@ if (isset($_POST['submit'])) {
     $body = "Nom: $name <br> Email: $email <br> Téléphone: $phone <br> Message: <br> $message";
 
     if (mail($to, $subject, $body, $headers)) {
-        echo 'Votre message a été envoyé avec succès.';
+        http_response_code(200); // Code de réponse HTTP 200 (OK)
     } else {
-        echo 'Une erreur s\'est produite lors de l\'envoi de votre message.';
+        http_response_code(500); // Code de réponse HTTP 500 (Internal Server Error)
     }
 }
 ?>
